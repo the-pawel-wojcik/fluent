0e1a70f4f3b2aad713c4784b08ceb573ab6518dd
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-01-24 22:21:52 -0800

    Ch11 extend the vector example

---
+++ src/ch11/readme.md
@@ -0,0 +1,13 @@
+# Links
+
+[Julien Danjou "Static, Class, Abstract Methods"](https://fpy.li/11-2)
+
+
+## Format Specifier
+[Format Specification Minilanguage](https://fpy.li/11-3)
+
+[fmtspec](https://fpy.li/fmtspec)
+
+[Formatted String Literals](https://fpy.li/11-4)
+
+[Format String Syntax](https://fpy.li/11-5)
--- src/ch11/vector2d_intro.py
+++ src/ch11/vector2d_intro.py
@@ -14,13 +14,8 @@ Test vector::
     True
     >>> print(v1)
     (3.0, 4.0)
-    >>> octets = bytes(v1)
-    >>> octets
-    b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
-    >>> abs(v1)
-    5.0
-    >>> bool(v1), bool(Vector2d(0, 0))
-    (True, False)
+    >>> {Vector2d(1, 0), Vector2d(0, 1)} # hash supported
+    {Vector2d(1.0, 0.0), Vector2d(0.0, 1.0)}
 """
 from array import array
 import math
@@ -29,8 +24,22 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -43,6 +52,10 @@ class Vector2d:
         return str(tuple(self))
 
     def __bytes__(self):
+        """
+        >>> bytes(Vector2d(3.0, 4.0))
+        b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
+        """
         return (
             bytes([ord(self.typecode)]) + bytes(array(self.typecode, self))
         )
@@ -51,8 +64,49 @@ class Vector2d:
         return tuple(self) == tuple(other)
 
     def __abs__(self):
+        """
+        >>> abs(Vector2d(3, 4))
+        5.0
+        """
         return math.hypot(self.x, self.y)
 
     def __bool__(self):
+        """
+        >>> bool(Vector2d(-1, 10))
+        True
+        >>> bool(Vector2d(0, 0))
+        False
+        """
         return bool(abs(self))
 
+    def __format__(self, format_spec: str, /) -> str:
+        """
+        >>> format(Vector2d(1, 1), '.2f')
+        '(1.00, 1.00)'
+        >>> format(Vector2d(1, 1), '.2fp')
+        '<r=1.41, φ=0.79>'
+        """
+        if format_spec.endswith('p'):
+            fmt = format_spec[:-1]
+            coords = (abs(self), math.atan2(self.y, self.x))
+            out_fmt = "<r={}, φ={}>"
+        else:
+            fmt = format_spec
+            coords = tuple(self)
+            out_fmt = "({}, {})"
+        components = (format(val, fmt) for val in coords)
+        return out_fmt.format(*components)
+    
+    def __hash__(self) -> int:
+        """
+        >>> hash(Vector2d(0, 1))
+        -1950498447580522560
+        """
+        return hash((self.x, self.y))
+
+    def __complex__(self) -> complex:
+        """
+        >>> complex(Vector2d(1, 1))
+        (1+1j)
+        """
+        return self.x + self.y * 1j
