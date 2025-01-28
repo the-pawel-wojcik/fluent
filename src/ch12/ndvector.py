98a73573674c49b5d2ba4337c0f7a5e4ef44c340
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-01-27 17:28:02 -0800

    Ch12 support Vector access with 'x'-'t' attributes

--- src/ch12/ndvector.py
+++ src/ch12/ndvector.py
@@ -1,9 +1,12 @@
 import reprlib
 import array
 import math
+import operator
+from typing import Any
 
 class Vector:
     typecode = 'd'
+    __match_attr__ = ('x', 'y', 'z', 't')
 
     def __init__(self, elements):
         """
@@ -53,6 +56,79 @@ class Vector:
         False
         """
         return bool(abs(self))
+
+    def __len__(self):
+        return len(self._content)
+
+    def __getitem__(self, key):
+        """
+        >>> v1 = Vector(range(10))
+        >>> v1[5]
+        5.0
+        >>> v1[:5:2]
+        Vector([0.0, 2.0, 4.0])
+        >>> v1[-1:]
+        Vector([9.0])
+        """
+        if isinstance(key, slice):
+            cls = type(self)
+            return cls(self._content[key])
+        idx = operator.index(key)
+        return self._content[idx]
+
+    def __getattr__(self, key):
+        """
+        >>> v = Vector([0, 1, 3])
+        >>> v.x
+        0.0
+        >>> v.t
+        Traceback (most recent call last):
+        ...
+        AttributeError: 'Vector' object has no attribute 't'
+        >>> v.ł
+        Traceback (most recent call last):
+        ...
+        AttributeError: 'Vector' object has no attribute 'ł'
+        """
+        cls = type(self)
+        try:
+            pos = self.__match_attr__.index(key)
+        except ValueError:
+            pos = -1
+        if 0 <= pos < len(self._content):
+            return self._content[pos]
+
+        msg = f'{cls.__name__!r} object has no attribute {key!r}'
+        raise AttributeError(msg)
+
+    def __setattr__(self, name: str, value: Any, /) -> None:
+        """
+        >>> v1 = Vector([1])
+        >>> v1.x = 5
+        Traceback (most recent call last):
+        ...
+        AttributeError: Read-only attribute 'x'
+        >>> v1.g = 5
+        Traceback (most recent call last):
+        ...
+        AttributeError: Can't set attributes 'a'-'z' in 'Vector'
+        >>> v1.Ł = 5
+        """
+        cls = self.__class__
+        if len(name) == 1:
+            if name in self.__match_attr__:
+                msg = f'Read-only attribute {name!r}'
+            elif name.islower():
+                msg = f"Can't set attributes 'a'-'z' in {cls.__name__!r}"
+            else:
+                msg = ''
+            if msg:
+                raise AttributeError(msg)
+
+        super().__setattr__(name, value)
+        
+        
+        
 
     @classmethod
     def frombytes(cls, bts):
--- src/ch12/readme.md
+++ src/ch12/readme.md
@@ -5,3 +5,5 @@
 [Radim Řehůřek gensim](https://fpy.li/12-2)
 
 [protocols: structural subtyping](https://fpy.li/pep544)
+
+[Travis Oliphant "Using Any Object For Slicing"](https://fpy.li/pep357)
