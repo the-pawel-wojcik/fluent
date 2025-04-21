fb4d3979481e9657463aa225c81f2f8d243128b7
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-04-21 09:46:55 -0400

    ch22 oscon.capitalize() @property

--- src/ch22/src/oscon/explore.py
+++ src/ch22/src/oscon/explore.py
@@ -38,6 +38,15 @@ from collections import abc
 
 
 class FrozenJSON:
+    def __new__(cls, arg):
+        if isinstance(arg, abc.Mapping):
+            return super().__new__(cls)
+        elif isinstance(arg, abc.MutableSequence):
+            return [cls(item) for item in arg]
+        else:
+            return arg
+
+
     def __init__(self, input):
         self.__data = input
 
